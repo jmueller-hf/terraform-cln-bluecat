78c81785b62372d5213b08aa27767fa2facf889e
jmueller-hf <122904141+jmueller-hf@users.noreply.github.com>
2023-02-09 15:59:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "execbluecat"{
   provisioner "local-exec"{
-    command = "python main.py --hostname \"${var.hostname}\" --value \"${var.value}\" --svcPassword \"${var.password}\""
+    command = "python3 main.py --hostname \"${var.hostname}\" --value \"${var.value}\" --svcPassword \"${var.password}\""
   }
 }
