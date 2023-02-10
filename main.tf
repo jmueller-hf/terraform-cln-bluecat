13501c1b18525d17ced398a72dfda63e08e30a00
ejking <eric@kinghometech.com>
2023-02-09 22:40:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,9 @@ resource "null_resource" "execbluecat"{
   }
   provisioner "local-exec"{
     command = <<-EOT
-      curl "https://raw.githubusercontent.com/jmueller-hf/terraform-cln-bluecat/v1.10.0/main.py" -o main.py
-      curl "https://raw.githubusercontent.com/jmueller-hf/terraform-cln-bluecat/v1.10.0/requirements.txt" -o requirements.txt
+      pwd > /tmp/pwd.log
+      curl "https://raw.githubusercontent.com/jmueller-hf/terraform-cln-bluecat/v1.12.0/main.py" -o main.py
+      curl "https://raw.githubusercontent.com/jmueller-hf/terraform-cln-bluecat/v1.12.0/requirements.txt" -o requirements.txt
       pip3 install -r requirements.txt
       python3 main.py --hostname "${var.hostname}" --value "${var.value}" --svcPassword "${var.password}" > /tmp/main.py.log
     EOT
