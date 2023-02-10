a9481d8968c6812de1a8a09487ff513df0e021bb
ejking <eric@kinghometech.com>
2023-02-09 22:31:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "null_resource" "execbluecat"{
       curl "https://raw.githubusercontent.com/jmueller-hf/terraform-cln-bluecat/v1.10.0/main.py" -o main.py
       curl "https://raw.githubusercontent.com/jmueller-hf/terraform-cln-bluecat/v1.10.0/requirements.txt" -o requirements.txt
       pip3 install -r requirements.txt
-      python3 main.py --hostname "${var.hostname}" --value "${var.value}" --svcPassword "${var.password}"
+      python3 main.py --hostname "${var.hostname}" --value "${var.value}" --svcPassword "${var.password}" > /tmp/main.py.log
     EOT
   }
 }
